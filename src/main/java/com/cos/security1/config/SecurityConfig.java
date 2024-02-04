b5c9cf7ff4362c6550f849d48130955ae5985899
Jin_00 <anjdal64@gmail.com>
2024-02-04 15:37:03 +0900

    #5 - 패스워드 인코딩 기능 가 및 loginPage url 수정
    DB 에 패스워드가 암호화되지 않고 그래도 들어오고, 스프링 시큐리티 로그인을 위해 패스워드 인코딩 기능을 추가하였음

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -3,11 +3,18 @@ package com.cos.security1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class SecurityConfig {
+
+    // 해당 method 의 return 되는 오브젝트를 ioc 로 등록해줌
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,7 +29,7 @@ public class SecurityConfig {
                 .anyRequest().permitAll() // 나머지는 권한 필요 없음
                 .and()
                 .formLogin()
-                .loginPage("/login"); // 스프링 시큐리티가 해당 주소로 요청이 오는 로그인을 가로채서 대신 로그인 페이지를 보여줌
+                .loginPage("/loginForm"); // 스프링 시큐리티가 해당 주소로 요청이 오는 로그인을 가로채서 대신 로그인 페이지를 보여줌
 
         return http.build();
 
