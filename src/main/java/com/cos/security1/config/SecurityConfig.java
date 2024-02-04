1597521c3295be433d5d4e944b49c8275e8de559
Jin_00 <anjdal64@gmail.com>
2024-02-04 20:58:56 +0900

    #11 - filter 경로 설정 및 순환참조 문제 해결
    BCryptPasswordEncoder 의 순환 참조 문제가 발생하여, Application class 로 이동해주었음

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,11 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
 public class SecurityConfig {
 
-    // 해당 method 의 return 되는 오브젝트를 ioc 로 등록해줌
-    @Bean
-    public BCryptPasswordEncoder encodePwd() {
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,7 +33,13 @@ public class SecurityConfig {
                 .formLogin()
                 .loginPage("/loginForm") // 스프링 시큐리티가 해당 주소로 요청이 오는 로그인을 가로채서 대신 로그인 페이지를 보여줌
                 .loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해 줍니다.
-                .defaultSuccessUrl("/"); // 로그인이 성공하면 해당 주소로 이동
+                .defaultSuccessUrl("/") // 로그인이 성공하면 해당 주소로 이동
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm")// 구글 로그인이 완료된 뒤의 후처리가 필요함.
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
+
         return http.build();
 
     }
